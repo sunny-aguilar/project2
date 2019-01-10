118962cd4bf4741af4f118a6475d8b02bf10aa6c
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-09 21:24:56 -0800

    project2

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 /*********************************************************************
 ** Author:          Sandro Aguilar
 ** Date:            Feb 2019
-** Description:     Zoo Tycoon - 
-**
-**
-**
+** Description:     Zoo Tycoon - this game allows players to run a zoo
+**                  business with different types of animals. The
+**                  animals have prices, maintenance costs, and return
+**                  a project each day.
 **
 *********************************************************************/
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
 
     return 0;
 }

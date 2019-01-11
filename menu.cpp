70d225b1d18510d2c997d1fcf796192fe568d62f
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-10 22:17:21 -0800

    project2

--- menu.cpp
+++ menu.cpp
@@ -10,11 +10,11 @@
 #include "menu.hpp"
 
 void Menu::mainMenu() {
-    cout << "---------------------------------\n";
+    cout << "----------------------------------\n";
     cout << " (\\ _ /)       /\\__/\\       ^---^\n";
     cout << " (' x ')      (=^.^=)      ( `o`)\n";
     cout << " c(\")(\")       (\")(\")_/    ( uu )\n";
-    cout << "---------------------------------\n";
+    cout << "----------------------------------\n";
     cout << "||| Welcome To Zoo Tycoon |||\n";
     cout << "1. Play the Game\n";
     cout << "2. Exit the Game\n";
